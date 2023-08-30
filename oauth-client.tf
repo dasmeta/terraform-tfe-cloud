739ad17f2070f249e899450f0487a61f895f14a6
Karapetyan, Aram <aram@dasmeta.com>
2023-08-30 10:57:33 +0400

    chore(DMVP-2598): specify backend type as it does not work without

--- oauth-client.tf
+++ oauth-client.tf
@@ -3,7 +3,7 @@ resource "tfe_oauth_client" "this" {
   name             = "git-oauth-client"
   organization     = var.org # this one is terraform cloud organisation
   service_provider = local.scm_providers[var.git_provider].provider
-  api_url          = local.scm_providers[var.git_provider].api_url
   http_url         = local.scm_providers[var.git_provider].http_url
+  api_url          = local.scm_providers[var.git_provider].api_url
   oauth_token      = var.git_token
 }
