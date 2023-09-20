a9b246985a8b74c6eacdcdba38c4c43fbb839e3a
Karapetyan, Aram <aram@dasmeta.com>
2023-09-20 18:06:06 +0400

    fix(DMVP-0000): pass tfc_token otherwise provider will not be able to pull variable set

--- modules/variable-set-reader/variables.tf
+++ modules/variable-set-reader/variables.tf
@@ -7,3 +7,9 @@ variable "org" {
   type        = string
   description = "The terraform cloud organization name where variable set and variables will be created"
 }
+
+variable "tfc_token" {
+  type        = string
+  description = "Terraform Cloud token so that workspace can load variable set values."
+  sensitive   = true
+}
